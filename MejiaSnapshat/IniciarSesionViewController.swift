583759ed9d3ede73ea0d0a12803e05baad972017
Rodrigo Mejia <rodrigoemdiaz@hotmail.com>
2023-06-04 19:32:13 -0500

    Inicio sesión con Google

--- MejiaSnapshat/IniciarSesionViewController.swift
+++ MejiaSnapshat/IniciarSesionViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class IniciarSesionViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -28,7 +29,25 @@ class IniciarSesionViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func loginGoogle() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in guard error == nil else { return }
+            guard let user = result?.user, let idToken = user.idToken?.tokenString
+            else { return }
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            Auth.auth().signIn(with: credential) {result, error in print("Se ha iniciado sesión con Google: \(String(describing: result))")}
+        }
+        
+    }
 
-
+    @IBAction func iniciarSesionGoogleTapped(_ sender: Any) {
+        loginGoogle()
+    }
+    
 }
 
